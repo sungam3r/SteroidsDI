e324c374b4a848a3917fbd4ef062c07de0196857
Michal Ciechan <lndcobra@hotmail.com>
2020-06-25 21:46:55 +0100

    Adding Covariant Interface (#9)

--- SteroidsDI.Core/Defer.cs
+++ SteroidsDI.Core/Defer.cs
@@ -1,12 +1,12 @@
 namespace System // yes, this is exactly the namespace by analogy with Func<T>
 {
     /// <summary>
-    /// An analogue of <see cref="Func{TResult}" /> for deferred resolve of the required object from a DI container.
+    /// An analogue of <see cref="Func{TResult}" /> for deferred resolving of the required object from a DI container.
     /// Has a more explicit/simple API compared to Func. This type must be used for those constructor arguments 
     /// that requires deferred calculation of T.
     /// </summary>
     /// <typeparam name="T"> Dependency type. </typeparam>
-    public abstract class Defer<T>
+    public abstract class Defer<T> : IDefer<T>
     {
         /// <summary> Get required dependency. </summary>
         public abstract T Value { get; }
---
+++ SteroidsDI.Core/IDefer.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Covariant interface for <see cref="Defer{T}" /> which is an analogue of <see cref="Func{TResult}" />
+    /// for deferred resolving of the required object from a DI container.
+    /// Has a more explicit/simple API compared to Func. This type must be used for those constructor arguments
+    /// that requires deferred calculation of T.
+    /// </summary>
+    /// <typeparam name="T"> Dependency type. </typeparam>
+    public interface IDefer<out T>
+    {
+        /// <summary> Get required dependency. </summary>
+        T Value { get; }
+    }
+}
