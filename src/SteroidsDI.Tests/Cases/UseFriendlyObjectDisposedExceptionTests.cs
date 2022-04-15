1ff7b7ba02721cc67012ad1093bd0cf2432e4864
Ivan Maximov <sungam3r@yandex.ru>
2022-04-15 17:39:10 +0300

    fix message

--- src/SteroidsDI.Tests/Cases/UseFriendlyObjectDisposedExceptionTests.cs
+++ src/SteroidsDI.Tests/Cases/UseFriendlyObjectDisposedExceptionTests.cs
@@ -33,7 +33,7 @@ internal class UseFriendlyObjectDisposedExceptionTests
                 ex.Message.ShouldBe(@"ObjectDisposedException occurred while resolving service 'ScopedService' by scoped service provider obtained from 'SteroidsDI.GenericScopeProvider`1[[SteroidsDI.Tests.Cases.UseFriendlyObjectDisposedExceptionTests, SteroidsDI.Tests, Version=1.0.3.0, Culture=neutral, PublicKeyToken=null]]'.
 Most likely this happened because the scope (and scoped provider as well) was disposed BEFORE the actual completion of the user code.
 Often, this is due to the forgotten 'await' operator somewhere in the user code. Make sure you await all the created tasks correctly.
-You see this message because 'ServiceProviderAdvancedOptions.UseFriendlyObjectDisposedException' is allowed.");
+You see this message because 'ServiceProviderAdvancedOptions.UseFriendlyObjectDisposedException' option is enabled.");
                 ex.InnerException.ShouldBeOfType<ObjectDisposedException>().Message.ShouldBe(@"Cannot access a disposed object.
 Object name: 'IServiceProvider'.");
             });
