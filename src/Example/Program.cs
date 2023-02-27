82a35a8e7fa96200094f6c0dd8810d179625b056
Ivan Maximov <sungam3r@yandex.ru>
2023-02-27 19:22:21 +0300

    top-level statements

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -1,10 +1,7 @@
-namespace Example;
+using Example;
 
-public class Program
-{
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+    .Build()
+    .Run();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
-}
